5e99bd42fe8be22149d793edc6e135129ab46374
JackWong992 <jackwong992@163.com>
2018-09-26 09:54:23 +0800

    add homeheader

---
+++ src/pages/home/components/HomeHeader.vue
@@ -0,0 +1,54 @@
+<template>
+   <div class="header">
+     <div class="header-left">
+       <div class="iconfont back-icon">&#xe624;</div>
+     </div>
+     <div class="header-input">
+       <div class="iconfont search-icon">&#xe632;</div>
+       输入城市/景点/游玩主题</div>
+     <div class="header-right">城市
+      <div class="iconfont city-icon">&#xe807;</div>
+     </div>
+   </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeHeader"
+    }
+</script>
+
+<style scoped lang="stylus">
+  @import "~styles/varibles.styl"
+  .header
+    line-height: .86rem
+    background: $bgColor
+    display: flex
+    .header-input
+      flex: 1
+      background: #fff
+      height: .64rem
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      border-radius: .06rem
+      padding-left: .4rem
+      .search-icon
+        display: inline-block
+    .header-left
+      float: left
+      width: .64rem
+      color: #fff
+      .back-icon
+        margin-left: .2rem
+        font-size: .4rem
+    .header-right
+      float: right
+      width: 1.28rem
+      text-align: center
+      color: #fff
+      .city-icon
+        display: inline-block
+        font-size: .24rem
+        margin-left: -.04rem
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home/Home'
+import '@/assets/styles/reset.css'
+import '@/assets/styles/border.css'
+import '@/assets/styles/iconfont.css'
+import fastClick from 'fastclick'
 
 Vue.use(Router)
+fastClick.attach(document.body)
+
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+     path: '/',
+     name: 'Home',
+     component: Home
     }
   ]
 })
