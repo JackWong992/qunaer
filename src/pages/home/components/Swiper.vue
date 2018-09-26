36206fc2f9c25e87f3341d33da02ec4eba08a468
JackWong992 <jackwong992@163.com>
2018-09-26 15:07:58 +0800

    完成首页导航

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="wrapper">
+      <swiper :options="swiperOption" >
+        <!-- slides -->
+        <swiper-slide v-for="(item,index) in swiperList" :key="index">
+          <img :src="item.imgUrl" class="swiper-img">
+        </swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeSwiper",
+      data() {
+        return {
+          swiperOption: {
+            pagination: '.swiper-pagination',
+            loop: true,
+            autoplay: 3500
+          },
+          swiperList: [{
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/87/534cf2e0a71fff02.jpg_750x200_5c778719.jpg'
+          },{
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/dc/61caf680f5ddbc02.jpg_750x200_c619ed45.jpg'
+          },{
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/83/a77595fb02eed602.jpg_750x200_1627b072.jpg'
+          },{
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/83/a77595fb02eed602.jpg_750x200_1627b072.jpg'
+          },{
+            imgUrl:'http://img1.qunarzz.com/piao/fusion/1809/6a/847ac7b98f10e202.jpg_750x200_f0593cb5.jpg'
+          }]
+        }
+      },
+    }
+</script>
+
+<style scoped lang="stylus">
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%;
+    height:0
+    padding-bottom: 27.25%
+    .swiper-img
+      width: 100%
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import Home from '@/pages/home/Home'
+import 'swiper/dist/css/swiper.css'
 import '@/assets/styles/reset.css'
 import '@/assets/styles/border.css'
 import '@/assets/styles/iconfont.css'
@@ -8,7 +10,7 @@ import fastClick from 'fastclick'
 
 Vue.use(Router)
 fastClick.attach(document.body)
-
+Vue.use(VueAwesomeSwiper)
 
 export default new Router({
   routes: [
