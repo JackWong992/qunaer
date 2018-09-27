d6de80a3c8faa14b8d9c24dfae9181d3f89c2916
JackWong992 <jackwong992@163.com>
2018-09-27 10:29:07 +0800

    完成城市头部组件

--- src/pages/City/components/Header.vue
+++ src/pages/City/components/Header.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <div class="header">
+      <router-link to="/">
+        <div class="iconfont city-icon">&#xe624;</div>
+      </router-link>
       城市选择
     </div>
   </div>
@@ -15,10 +18,18 @@
 <style scoped lang="stylus">
   @import '~styles/varibles.styl'
   .header
+    position: relative
     height: .86rem
     line-height: .86rem
     text-align: center
     background: $bgColor
     color: #fff
     overflow: hidden
+    font-size: .32rem
+    .city-icon
+      width: .64rem
+      position: absolute
+      color: #fff
+      top: 0
+      left: 0
 </style>
