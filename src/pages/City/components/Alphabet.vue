3b67032cdeb30b79b62de26d432ae4a5692d2e0d
JackWong992 <jackwong992@163.com>
2018-09-27 16:50:48 +0800

    完成首页字母列表布局

---
+++ src/pages/City/components/Alphabet.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+    export default {
+        name: "CityAlphabet",
+    }
+</script>
+
+<style scoped lang="stylus">
+  @import "~styles/varibles.styl"
+  .list
+    width: .4rem
+    position: absolute
+    right: 0
+    bottom: 0
+    top: 1.58rem
+    /*background: red*/
+    display: flex
+    flex-direction: column
+    justify-content: center
+    align-content: center
+    .item
+      text-align: center
+      line-height: .44rem
+      color: $bgColor
+</style>
