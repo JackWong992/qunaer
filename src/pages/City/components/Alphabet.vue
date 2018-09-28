fa6c6687eaf6ef21c63c1928af04d7c95e8c0b84
JackWong992 <jackwong992@163.com>
2018-09-28 08:45:30 +0800

    完成列表滚动逻辑

--- src/pages/City/components/Alphabet.vue
+++ src/pages/City/components/Alphabet.vue
@@ -1,24 +1,67 @@
 <template>
   <ul class="list">
     <li class="item"
-        v-for="(item,key) in cities" :key="key"
-      @click="handleClickLetter">{{key}}</li>
+        v-for="item in letters" :key="item"
+      :ref="item"
+      @click="handleClickLetter"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+    >{{item}}</li>
   </ul>
 </template>
 
 <script>
     export default {
-        name: "CityAlphabet",
+      name: "CityAlphabet",
         props: {
           cities: {
             type: Object
           }
         },
+      data() {
+        return {
+          touchStatus: false  //状态判断,开始滑动状态为true，结束为false
+        }
+      },
       methods: {
           handleClickLetter(e){
             console.log('弟弟传来的值：'+ e.target.innerText )
             this.$emit( 'change', e.target.innerText )
+          },
+          handleTouchStart(){
+            this.touchStatus = true
+            //console.log('开始滚动了....')
+          },
+          handleTouchMove(e){
+           // console.log('滚动了....')
+            if(this.touchStatus){
+              const startY = this.$refs['A'][0].offsetTop
+              //A到当前城市的距离
+              console.log('A字母到当前城市的距离:'+startY)
+              const touchY = e.touches[0].clientY-79 //滚动的距离距离当前城市的距离
+              //79为粉色边框顶部的长度，touchY是距离当前城市的距离
+              //console.log('touchY:'+touchY)
+              const index =  Math.floor( (touchY-startY)/20 )
+              if( index >= 0 && index <= this.letters.length ){
+                this.$emit('change', this.letters[index] )
+              }
+            }
+          },
+          handleTouchEnd(){
+            this.touchStatus = false
+            //console.log('滚动结束了....')
           }
+      },
+      computed: {
+        letters(){
+          const letters = []
+          for( let i in this.cities ){
+            letters.push(i)
+          }
+          return letters
+        //制造一个数组，数组的内容为：['a','b','c',...'z']
+        }
       }
     }
 </script>
