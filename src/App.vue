fabcaa6472266cb12015f95ee7f080594335b1bb
JackWong992 <jackwong992@163.com>
2018-09-29 09:37:20 +0800

    利用keep-alive提高页面性能加载优

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <router-view/>
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
+    <!-- 路由内容加载一次以后就不会重新加载，直接从内容中调取数据，提高性能优化 -->
   </div>
 </template>
 
